f9e63b57fe2257e44dd297b5deae67f6540ae253
nodojo <nulldojo@gmail.com>
2024-06-17 22:33:50 -0400

    review general types, structs, maps, slices, arrays, and custom types

--- main.go
+++ main.go
@@ -2,38 +2,127 @@ package main
 
 import "fmt"
 
-// global variables declared outside of functions
-//var name = "foo" // direct assignment, type is automatically inferred (this is only possible in the global scope when using the keyword var)
-//var firstName string = "foo" // direct assignment, type is explicitly defined
-//var lastName string // initialized as the default value of the type (empty string in this case)
-
-// preferred declaration format versus the declaration above
+// // GENERAL TYPES
 // var (
-// 	name             = "foo"
-// 	firstName string = "foo"
-// 	lastName  string
+// 	floatVar32 float32 = 0.1
+// 	floatVar64 float64 = 0.1
+// 	name       string  = "foo"
+// 	intVar32   int32   = 1
+// 	intVar64   int64   = 48484
+// 	intVar     int     = -1
+// 	uintVar    uint    = 1   // unsigned integer, cannot be negative
+// 	uint32Var  uint32  = 1   // 4 bytes
+// 	uint64Var  uint64  = 1   // 8 bytes
+// 	unit8Var   uint8   = 0x1 // 1 byte
+// 	byteVar    byte    = 0x2 // exactly the same as a unit8
+// 	runeVar    rune    = 'a' // specified with single quotes (so that it's a rune and not a string), if you index a string you're going to also have a rune
 // )
 
-// constants are almost always only defined in the global namespace
-// const version = 1
+// // STRUCTS (an empty struct doesn't occupy any memory)
+// // golang initializes empty variables to the default state of that type of variable
+// type Player struct {
+// 	name        string
+// 	health      int
+// 	attackPower float64
+// }
 
-// constant variable names always begin with a lowercase character
-const (
-	version = 1
-	keyLen  = 10
-)
+// // when you attach a function to a struct it's called a function receiver
+// func (player Player) getHealth() int {
+// 	return player.health
+// }
+
+// first struct example
+// notice param format is variable name the its type in golang
+// func getHealth(player Player) int {
+// 	return player.health
+// }
+
+// // CUSTOM TYPE
+// // telling compiler that I have a type that is called weapon and it's giong to be a string
+// type Weapon string
+
+// func getWeapon(weapon Weapon) string {
+// 	// cast weapon to string to return (which is type Weapon (not string... even though the underlying type is string))
+// 	return string(weapon)
+// }
 
 func main() {
-	// local variables don't require you to use the var keyword
-	// version := 1           // the := operator means the type will be inferred (int in this case)
-	// otherVersion := "bar"  // inferred as a string by the compiler
-	// anotherVersion := 10.1 // inferred as a float32 or float64 (float32 in this case due to the small size of the number)
+	// ARRAYS
+	// the size of an array remains static (unlike a slice's size which is dynamic)
+	// numbers := [2]int{}
+	// numbers[0] = 1
+	// numbers[1] = 2
 
-	// fmt.Println(version)
-	// fmt.Println(otherVersion)
-	// fmt.Println(anotherVersion)
+	// SLICES
+	// an empty slice like this is almost always how you declare and use a slice
+	numbers := []int{} // empty
+	// numbers := []int{1, 2, 3}
+	// when using the make keyword, we need to specify the initial size (2 in this example) of the slice (slices are dynamic, so they can expand/contract)
+	// almost never need to use this
+	// otherNumbers := make([]int, 2)
+	otherNumbers := make([]int, 0) // empty
 
-	// var version int
+	// add to a slice
+	// numbers = append(numbers, 1)
+	// numbers = append(numbers, 10)
 
-	fmt.Println(version)
+	// looping over a slice would be done using the same for/range loop that we used for maps
+
+	fmt.Println(numbers)
+	fmt.Println(otherNumbers)
+
+	// MAPS
+	// majority of the time, init a map like this (possibly the most idiomatic)
+	// users := map[string]int{} //empty map
+	// users := map[string]int{
+	// 	"anothony": 36,
+	// }
+	// // the make built-in function makes a new default for certain types in golang
+	// users := make(map[string]int) // hey go compiler, make me a map of strings to integers
+	// users["foo"] = 10
+	// users["bar"] = 11
+	// delete by key from map... only applicable to maps
+	// delete(users, "foo")
+
+	// // range over a map (key and value)
+	// for k, v := range users {
+	// 	// %s = string, %d = integers
+	// 	fmt.Printf("the key %s and the value %d\n", k, v)
+	// }
+
+	// // range over a map (keys only)
+	// for key := range users {
+	// 	fmt.Printf("the key %s ", key)
+	// }
+
+	// age := users["foo"]
+	// since "baz" doesn't exist, it will output the default value of an integer (0)
+	// this built-in go behvior can come back to shoot you in the foot, because 0 could be misinterpreted as value that has been set (not by default)
+	// age := users["baz"]
+	// this prevents the built-in behavior that defaults an integer to 0 from shooting you in the foot
+	// age, ok := users["baz"]
+	// if !ok {
+	// 	fmt.Println("baz doesn't exist in the map")
+	// } else {
+	// 	fmt.Println("baz exists in the map: ", age)
+	// }
+	// fmt.Printf("age: %+v\n", age)
+
+	// fmt.Printf("users: %+v\n", users)
+
+	// STRUCTS
+	// player := Player{
+	// 	name:        "captain jack",
+	// 	health:      101,
+	// 	attackPower: 45.1,
+	// }
+
+	// print formatted
+	// %v = verbose, %+v outputs property names in struct along with the values ("super" verbose mode)
+	// fmt.Printf("this is the player: %+v\n", player)
+	// %d is for integers
+	// fmt.Printf("health: %d\n", getHealth(player))
+	// fmt.Printf("health: %d\n", player.getHealth())
+	// getHealth() was just to create an example, you don't need a getter because you can just access the same value with player.health
+	// fmt.Printf("health: %d\n", player.health)
 }
