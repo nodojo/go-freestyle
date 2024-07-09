6f269fde6d1c59e07e3df71a18538b6de2d9219f
nodojo <nulldojo@gmail.com>
2024-07-08 23:04:29 -0400

    save point before implementing putter

--- main.go
+++ main.go
@@ -5,17 +5,15 @@ package main
 // * these represent the composability of your program
 // try to keep your interfaces as short as possible
 
+// so, this Storage interface could end up implementing 20 different function methods...
+// in our updateValue() function below, we only need access to one (Put)
 type Storage interface {
-	// this is going to be get by id
-	// Get(int) (any, error)
-	// you don't have to specify id here, but you can and it will be clearer and more verbose
 	Get(id int) (any, error)
 	Put(id int, val any) error
 }
 
 type FooStorage struct{}
 
-// s is a pointer to FooStorage
 func (s *FooStorage) Get(id int) (any, error) {
 	return nil, nil
 }
@@ -24,23 +22,30 @@ func (s *FooStorage) Put(id int, val any) error {
 	return nil
 }
 
+type BarStorage struct{}
+
+func (s *BarStorage) Get(id int) (any, error) {
+	return nil, nil
+}
+
+func (s *BarStorage) Put(id int, val any) error {
+	return nil
+}
+
 type Server struct {
 	store Storage
 }
 
-func updateValue(id int, val any) error {
-	// problem here is the dependency we have within our function -> this reference to FooStorage...
-	// the way it's written here, we are always going to create FooStorage with this implementation
-	store := &FooStorage{}
+// now we use dependency injection to remove the dependency
+// this would be the way we could cleanly "plug & play" storage methods...
+// (ex: Postgres, MongoDB, testing (which is an "in-memory" store, so no need to connect over the network))
+func updateValue(id int, val any, store Storage) error {
 	return store.Put(id, val)
 }
 
 func main() {
 	s := &Server{
 		store: &FooStorage{},
-	} // instantiate
-	s.store.Get(1)
-	s.store.Put(1, "foo")
-	// if you run this, you'll get a runtime error because the Storage interface is empty...
-	// that's because Storage is the interface and not the actual implementation
+	}
+	updateValue(1, "foo", s.store)
 }
