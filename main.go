3566db5c836642b57b92659aea68a719634a9c45
nodojo <nulldojo@gmail.com>
2024-07-08 21:44:29 -0400

    struct embedding - example 1

--- main.go
+++ main.go
@@ -10,14 +10,26 @@ type Entity struct {
 	posy    int    //
 }
 
+type SpecialEntity struct {
+	Entity       // struct embedding
+	specialField float64
+}
+
 func main() {
-	// e := &Entity{ // declared as pointer
-	e := Entity{ // declared as literal
-		name:    "my entity",
-		id:      "id 1",
-		version: "version 1.1",
-		posx:    100,
-		posy:    200,
+	e := SpecialEntity{
+		specialField: 88.88,
+		Entity: Entity{
+			name:    "my special entity",
+			version: "1.1",
+			// id:      "my special id",
+			posx: 100,
+			posy: 200,
+		},
 	}
+
+	// alternatively, you can initialize the field individually
+	e.id = "my special id"
+
 	fmt.Printf("%+v\n", e)
+	// fmt.Printf("%+v\n", e.id)
 }
