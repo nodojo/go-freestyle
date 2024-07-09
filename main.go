9dabc9e8aaf760328ba44e4bd06da308b15bca6a
nodojo <nulldojo@gmail.com>
2024-07-08 22:51:53 -0400

    highlight issue created by the dependency on FooStorage in the func updateValue

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 // 1. advanced interface mechanics
 // 2. typed functions
 // * these represent the composability of your program
+// try to keep your interfaces as short as possible
 
 type Storage interface {
 	// this is going to be get by id
@@ -12,12 +13,32 @@ type Storage interface {
 	Put(id int, val any) error
 }
 
+type FooStorage struct{}
+
+// s is a pointer to FooStorage
+func (s *FooStorage) Get(id int) (any, error) {
+	return nil, nil
+}
+
+func (s *FooStorage) Put(id int, val any) error {
+	return nil
+}
+
 type Server struct {
 	store Storage
 }
 
+func updateValue(id int, val any) error {
+	// problem here is the dependency we have within our function -> this reference to FooStorage...
+	// the way it's written here, we are always going to create FooStorage with this implementation
+	store := &FooStorage{}
+	return store.Put(id, val)
+}
+
 func main() {
-	s := &Server{} // instantiate
+	s := &Server{
+		store: &FooStorage{},
+	} // instantiate
 	s.store.Get(1)
 	s.store.Put(1, "foo")
 	// if you run this, you'll get a runtime error because the Storage interface is empty...
