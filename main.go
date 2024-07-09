56f1a5b3dc37f74e9873b490c2aab4c64cbf45b6
nodojo <nulldojo@gmail.com>
2024-07-08 21:36:54 -0400

    create basic struct

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+type Entity struct {
+	name    string //
+	id      string //
+	version string //--> field members
+	posx    int    //
+	posy    int    //
+}
+
+func main() {
+	// e := &Entity{ // declared as pointer
+	e := Entity{ // declared as literal
+		name:    "my entity",
+		id:      "id 1",
+		version: "version 1.1",
+		posx:    100,
+		posy:    200,
+	}
+	fmt.Printf("%+v\n", e)
 }
