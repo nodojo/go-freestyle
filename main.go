24dfe59e06e854291719655488ec87f5749b7cf1
nodojo <nulldojo@gmail.com>
2024-07-08 23:14:25 -0400

    advanced interface mechanics example

--- main.go
+++ main.go
@@ -5,11 +5,22 @@ package main
 // * these represent the composability of your program
 // try to keep your interfaces as short as possible
 
+type Putter interface {
+	Put(id int, val any) error
+}
+
 // so, this Storage interface could end up implementing 20 different function methods...
 // in our updateValue() function below, we only need access to one (Put)
 type Storage interface {
 	Get(id int) (any, error)
-	Put(id int, val any) error
+	// Put(id int, val any) error
+	Putter
+}
+
+type simplePutter struct{}
+
+func (s *simplePutter) Put(id int, val any) error {
+	return nil
 }
 
 type FooStorage struct{}
@@ -36,16 +47,16 @@ type Server struct {
 	store Storage
 }
 
-// now we use dependency injection to remove the dependency
-// this would be the way we could cleanly "plug & play" storage methods...
-// (ex: Postgres, MongoDB, testing (which is an "in-memory" store, so no need to connect over the network))
-func updateValue(id int, val any, store Storage) error {
-	return store.Put(id, val)
+func updateValue(id int, val any, p Putter) error {
+	return p.Put(id, val)
 }
 
 func main() {
-	s := &Server{
-		store: &FooStorage{},
-	}
-	updateValue(1, "foo", s.store)
+	// s := &Server{
+	// 	store: &FooStorage{},
+	// }
+	// updateValue(1, "foo", s.store)
+
+	sputter := &simplePutter{}
+	updateValue(1, "foo", sputter)
 }
