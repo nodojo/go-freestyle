d2bf21ca18ce79296c98fc732c722f57f4fbd00a
nodojo <nulldojo@gmail.com>
2024-07-08 23:48:24 -0400

    typed functions as an argument

--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 // 1. advanced interface mechanics
 // 2. typed functions
 
-func main() {
+type TransformFunc func(s string) string
 
+func Uppercase(s string) string {
+	return strings.ToUpper(s)
+}
+
+func Prefixer(s string) string {
+	return "FOO_" + s
+}
+
+func transformString(s string, fn TransformFunc) string {
+	return fn(s)
+}
+
+func main() {
+	fmt.Println(transformString("hello Sailor!", Uppercase))
+	fmt.Println(transformString("hello Sailor!", Prefixer))
 }
