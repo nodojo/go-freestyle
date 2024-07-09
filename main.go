46ba38b7d01079a2e15d02e02be70b3eb00e1c64
nodojo <nulldojo@gmail.com>
2024-07-09 00:02:35 -0400

    when to use typed functions and when to use interfaces

--- main.go
+++ main.go
@@ -8,6 +8,14 @@ import (
 // 1. advanced interface mechanics
 // 2. typed functions
 
+// WHEN DO I USE TYPED FUNCTIONS AND WHEN DO I USE AN INTERFACE?
+// - use an interface if you want to have state (a typed function has no way to store state)
+type Foo interface{}
+
+type FooImplementation struct {
+	state any
+}
+
 type TransformFunc func(s string) string
 
 func Uppercase(s string) string {
