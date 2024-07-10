d1270f7e6f44e6fabd4f33bfc69621bd39f0cd66
nodojo <nulldojo@gmail.com>
2024-07-10 11:27:06 -0400

    add brackets and values to switch to generic

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 // we're gonna make this generic
-type CustomMap struct {
-	data map[string]int // example use: usernames with an id or age
+// in the brackets you're going to provide your generic types
+// our key (K) can be anything, but we need to say that's it's going to be a comparable...
+// ex: key in a hash map that needs to be compared to another key in a hash map to see if it exists
+type CustomMap[K comparable, V any] struct {
+	data map[K]V // example use: usernames with an id or age
 }
 
 // implement a function into this map to insert values
-func (m *CustomMap) Insert(k string, v int) error {
+func (m *CustomMap[K, V]) Insert(k K, v V) error {
 	// the problem is we have a big dependency... our map, because it's a string/int -> if we want to change this, we can't
 	// in other words, we're tied to the data types used by the map
 	// how are we going to fix this? generics
@@ -16,10 +19,10 @@ func (m *CustomMap) Insert(k string, v int) error {
 
 // make a new map to initialize the data
 // it's going to be a pointer to this CustomMap and it's going to return the pointer to the CustomMap
-func NewCustomMap() *CustomMap {
+func NewCustomMap[K comparable, V any]() *CustomMap[K, V] {
 	// the data inside of this map is going to be make me a map of string to int
-	return &CustomMap{
-		data: make(map[string]int),
+	return &CustomMap[K, V]{
+		data: make(map[K]V),
 	}
 }
 
