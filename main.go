c4633048e8da13499eca4da6fd2d5e3358d6bd73
nodojo <nulldojo@gmail.com>
2024-07-30 06:39:15 -0400

    alternate approach using a function receiver and initializing player as a pointer

--- main.go
+++ main.go
@@ -1,25 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // a pointer is an 8 byte long integer that points to a memory address
 type Player struct {
 	HP int
 }
 
-// accept pointer to player instead of passing a copy by value
-func TakeDamage(player *Player, amount int) {
-	player.HP -= amount
-	fmt.Println("player is taking damage. New HP ->", player.HP)
+// function receiver
+func (p *Player) TakeDamage(amount int) {
+	p.HP -= amount
+	fmt.Println("player is taking damage. New HP ->", p.HP)
 }
 
 func main() {
-	player := Player{
+	player := &Player{
 		HP: 100,
 	}
 
 	// send pointer to player location in memory
-	TakeDamage(&player, 10)
+	player.TakeDamage(10)
 
 	fmt.Printf("%+v\n", player)
 }
