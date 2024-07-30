bb331e4fcca2c5c8d33cfea5e3de368014fc17e0
nodojo <nulldojo@gmail.com>
2024-07-30 06:29:53 -0400

    passing a copy of player by value to TakeDamage() -> on line 23 player outputs as 100 (not 90) because the actual player has not been changed

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+// a pointer is an 8 byte long integer that points to a memory address
+type Player struct {
+	HP int
+}
+
+func TakeDamage(player Player, amount int) {
+	player.HP -= amount
+	fmt.Println("player is taking damage. New HP ->", player.HP)
+}
+
+func main() {
+	player := Player{
+		HP: 100,
+	}
+	// golang sends a copy of player as the param to TakeDamage()...
+	// which means, if player is not a pointer then we are adjusting the copy of the player, not the actual player
+	TakeDamage(player, 10)
+
+	fmt.Printf("%+v\n", player)
 }
