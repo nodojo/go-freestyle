30cc4c6e5fbcca2fcd992e040ac884e0578ea005
nodojo <nulldojo@gmail.com>
2024-07-30 06:49:40 -0400

    use pointers to increase performance

--- main.go
+++ main.go
@@ -7,17 +7,18 @@ type BigData struct {
 	// ..
 }
 
-func doSomethingWithData(data BigData) {
+func doSomethingWithData(data *BigData) {
 	// manipulate the data inside this function
 }
 
 func main() {
-	data := BigData{} // 1 gb
+	data := &BigData{} // 1 gb
 
 	// something like this would be bad -> not perfomant...
 	// which should reinforce a decision for us to use pointers
 	for i := 0; i < 10000; i++ {
 		// copy 1 gb of data inside if that function
+		// -> by using pointers instead, we will only be passing 8 bytes of pointer
 		doSomethingWithData(data)
 	}
 }
