d9156cdaee38efb4c0b55d171df37d7b65f4bbdd
nodojo <nulldojo@gmail.com>
2024-09-17 11:30:57 -0400

    sic

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"time"
 )
 
@@ -11,17 +10,13 @@ func main() {
 	// go routine is a function that is being scheduled by the go scheduler (in the go runtime... each time you build a binary, each time you build your own program, it's going to compile in the golang runtime)...
 	// the scheduler takes all your goroutines and it will make sure that if a goroutine has no task to perform, it's going to yield the processor and it's going to schedule something else...
 	// understand that in go, there is no such thing as parallelism (goroutines don't run in parallel), it's basically concurrency
-	result := fethcResource()
-	fmt.Println("result:", result)
+	// result := fetchResource()
+	// fmt.Println("result:", result)
 
-	fethcResource()
-	fethcResource()
-	fethcResource()
-	fethcResource()
-	fethcResource()
+	go fetchResource()
 }
 
-func fethcResource() string {
+func fetchResource() string {
 	time.Sleep(time.Second * 2)
 	return "some result"
 }
