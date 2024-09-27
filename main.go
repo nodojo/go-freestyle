bad90d5ca7226ece9cbbcb5ba5245167c14f84fb
nodojo <52790569+nodojo@users.noreply.github.com>
2024-09-27 03:11:32 -0400

    lock/unlock mutex

--- main.go
+++ main.go
@@ -7,12 +7,15 @@ import (
 // state -> set OR update OR delete (how we manipulate state)
 
 type State struct {
-	sync.Mutex
+	mu    sync.Mutex
 	count int
 }
 
 func (s *State) setState(i int) {
+	s.mu.Lock()
 	s.count = i
+	// note: every Lock() must have a Unlock() to free up the locked resources
+	s.mu.Unlock()
 }
 
 func main() {
