5a2c0b6c189c9933f2b8ccaa17fa5aa1cdacaa42
nodojo <52790569+nodojo@users.noreply.github.com>
2024-09-27 02:21:11 -0400

    simple initial example

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+// state -> set OR update OR delete (how we manipulate state)
+
+type State struct {
+	count int
+}
+
+func main() {
+	// initialize state (dont forget if the fields arent set, go will initialize them to the default values for their type)
+	state := State{}
+
+	for i := 0; i < 10; i++ {
+		state.count = i
+	}
+
+	fmt.Println(state)
 }
