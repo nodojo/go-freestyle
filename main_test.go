7a25f9194cf70b59f57b3e9272edebe3c10469cc
nodojo <52790569+nodojo@users.noreply.github.com>
2024-09-27 02:27:53 -0400

    add simple test

---
+++ main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestState(t *testing.T) {
+	state := State{}
+
+	for i := 0; i < 10; i++ {
+		state.count = i
+	}
+
+	fmt.Printf("%+v\n", state)
+}
