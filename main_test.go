358054b667c3c0aba5b85f1644ce2f4e362680ee
nodojo <52790569+nodojo@users.noreply.github.com>
2024-09-27 02:54:48 -0400

    implement wait group

--- main_test.go
+++ main_test.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 )
 
 func TestState(t *testing.T) {
 	state := State{}
+	wg := sync.WaitGroup{}
 
 	for i := 0; i < 10; i++ {
-		state.count = i
+		wg.Add(1) // does some work
+		go func(i int) {
+			state.count = i + 1
+			wg.Done() // done working
+		}(i)
 	}
 
+	wg.Wait()
 	fmt.Printf("%+v\n", state)
 }
