4c54c94201eda6889fcdc2134d63b0fa3cd01d18
nodojo <52790569+nodojo@users.noreply.github.com>
2024-11-09 02:51:00 -0500

    update to use goroutines and channels
    note deadlock error cause in comments

--- main.go
+++ main.go
@@ -23,43 +23,65 @@ func main() {
 	fmt.Println("fetching user profile took ", time.Since(start))
 }
 
+// we can make this better by using goroutines to fetch
+// the different parts of a user profile asynchronously
+type Response struct {
+	data any
+	err  error
+}
+
 func handleGetUserProfile(id int) (*UserProfile, error) {
-	comments, err := getComments(id)
-	if err != nil {
-		return nil, err
+	// create channel and buffer for the 3 responses (comments, likes, friends)
+	respch := make(chan Response, 3)
+
+	// schedule this in a goroutine
+	go getComments(id, respch)
+	go getLikes(id, respch)
+	go getFriends(id, respch)
+
+	// range over our response channel
+	// DEADLOCK -> because range "keeps ranging" and doesn't know when to exit
+	for resp := range respch {
+		fmt.Println(resp)
 	}
-	likes, err := getLikes(id)
-	if err != nil {
-		return nil, err
-	}
-	friends, err := getFriends(id)
-	if err != nil {
-		return nil, err
-	}
+
 	return &UserProfile{
-		ID:       id,
-		Comments: comments,
-		Likes:    likes,
-		Friends:  friends,
+		// ID:       id,
+		// Comments: comments,
+		// Likes:    likes,
+		// Friends:  friends,
 	}, nil
 }
 
-func getComments(id int) ([]string, error) {
+// we won't need the return because we are going to inject the data into the channel
+func getComments(id int, respch chan Response) {
 	time.Sleep(time.Millisecond * 200) // mimic http round trip
 	comments := []string{
 		"hey, that was great",
 		"yeah buddy",
 		"oh, I didn't know that",
 	}
-	return comments, nil
+	respch <- Response{
+		data: comments,
+		err:  nil,
+	}
 }
 
-func getLikes(id int) (int, error) {
+// we won't need the return because we are going to inject the data into the channel
+func getLikes(id int, respch chan Response) {
 	time.Sleep(time.Millisecond * 200) // mimic http round trip
-	return 33, nil
+	respch <- Response{
+		data: 11,
+		err:  nil,
+	}
 }
 
-func getFriends(id int) ([]int, error) {
+// we won't need the return because we are going to inject the data into the channel
+func getFriends(id int, respch chan Response) {
 	time.Sleep(time.Millisecond * 100) // mimic http round trip
-	return []int{11, 34, 854, 455}, nil
+	friendIds := []int{11, 34, 854, 455}
+	respch <- Response{
+		data: friendIds,
+		err:  nil,
+	}
 }
