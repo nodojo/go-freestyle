28fae233955f9817c1677d8655385a7415673ec8
nodojo <52790569+nodojo@users.noreply.github.com>
2024-11-08 23:57:24 -0500

    mimic http round trip with latency

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"time"
+)
 
+// package context is a standard library package
+func main() {
+	start := time.Now()
+	result, err := thirdpartyHTTPCall()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("the response took %v:%+v\n", time.Since(start), result)
+}
+
+// mimic an http round trip
+func thirdpartyHTTPCall() (string, error) {
+	// mimic latency
+	time.Sleep(time.Millisecond * 100)
+	return "some response", nil
 }
