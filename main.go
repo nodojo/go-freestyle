5684f292b13578ad30aa434a2e2862bd2f9f30ca
nodojo <52790569+nodojo@users.noreply.github.com>
2024-11-09 02:20:59 -0500

    spec out logic to get user response

--- main.go
+++ main.go
@@ -1,5 +1,63 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"time"
+)
 
+type UserProfile struct {
+	ID       int
+	Comments []string
+	Likes    int
+	Friends  []int
+}
+
+func main() {
+	userProfile, err := handleGetUserProfile(10)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(userProfile)
+}
+
+func handleGetUserProfile(id int) (*UserProfile, error) {
+	comments, err := getComments(id)
+	if err != nil {
+		return nil, err
+	}
+	likes, err := getLikes(id)
+	if err != nil {
+		return nil, err
+	}
+	friends, err := getFriends(id)
+	if err != nil {
+		return nil, err
+	}
+	return &UserProfile{
+		ID:       id,
+		Comments: comments,
+		Likes:    likes,
+		Friends:  friends,
+	}, nil
+}
+
+func getComments(id int) ([]string, error) {
+	time.Sleep(time.Millisecond * 200) // mimic http round trip
+	comments := []string{
+		"hey, that was great",
+		"yeah buddy",
+		"oh, I didn't know that",
+	}
+	return comments, nil
+}
+
+func getLikes(id int) (int, error) {
+	time.Sleep(time.Millisecond * 200) // mimic http round trip
+	return 33, nil
+}
+
+func getFriends(id int) ([]int, error) {
+	time.Sleep(time.Millisecond * 100) // mimic http round trip
+	return []int{11, 34, 854, 455}, nil
 }
