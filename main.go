826045b5801025ffe56c842963992c7e1f6a9c8d
nodojo <52790569+nodojo@users.noreply.github.com>
2024-11-11 21:59:33 -0500

    add comments

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// run to change the port to :7000:
+//
+//	./bin/api --listenAddr :7000
 func main() {
 	listenAddr := flag.String("listenAddr", ":5000", "the listen address of the api server")
 	flag.Parse()
